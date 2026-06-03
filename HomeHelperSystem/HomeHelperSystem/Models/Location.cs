b02bccff4d98e423c00cd93da076de372b5dbf09
gnedyalkov <gne8606@gmail.com>
2026-06-03 18:56:43 +0300

    Delete HomeHelperSystem directory

--- HomeHelperSystem/HomeHelperSystem/Models/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HomeHelperSystem.Models
-{
-    public class Location
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-    }
-}
